57bc69c2f1248ff2d529e74e44a2c46b4de50e58
pquoc693 <pquoc693@gmail.com>
2023-03-19 16:15:42 +0700

    nextjs-ecommerce

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,40 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import { SessionProvider, useSession } from "next-auth/react";
+import { StoreProvider } from "../utils/Store";
+import { useRouter } from "next/router";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <StoreProvider>
+        <PayPalScriptProvider>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </PayPalScriptProvider>
+      </StoreProvider>
+    </SessionProvider>
+  );
 }
+
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required");
+    }
+  });
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return children;
+}
+
+export default App;
